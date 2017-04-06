9e17ac37293acd877dbf10f087819db009ae4595
Eric Lindau <eric.lindau1@gmail.com>
2017-04-06 02:03:29 -0600

    Little School Bus (not completed)

--- Little School Bus/read.py
+++ Little School Bus/read.py
@@ -6,17 +6,18 @@ HEADER_OFFSET = int(sys.argv[1])
 with open("littleschoolbus.bmp", "rb") as img:
 	bytes = bytearray(img.read())
 
-bytes = bytes[HEADER_OFFSET:]
+# Should be from offset+1 onward
+bytes = bytes[HEADER_OFFSET+1:]
 
 buffer = ""
 
 bits = ""
 for byte in bytes:
-	if len(bits) < 8:
+	if len(bits) != 8:
 		bits += str(byte&1)
-	elif bits is not "00000000":
+	else:
 		char = chr(int(bits, 2))
-		if ord(char) > 64 and ord(char) < 123:
+		if ord(char) > 32 and ord(char) < 127:
 			buffer += char
 		bits = ""
 
