61cb6477d5380af0db71fb1387fe8e6fed9a3dfc
Eric Lindau <eric.lindau1@gmail.com>
2017-04-06 16:13:35 -0600

    Final update

--- Little School Bus/read.py
+++ Little School Bus/read.py
@@ -1,24 +1,34 @@
-import sys
+"""Utility that uses LSB decoding to decode a message found in the first
+45 bytes of the pixel data of a BMP file.
+"""
+# Offset for Windows BMP files, pre-determined by hex dump.
+BMP_HEADER_OFFSET = 54
 
-
-HEADER_OFFSET = int(sys.argv[1])
-
+# Dump all binary of BMP file.
 with open("littleschoolbus.bmp", "rb") as img:
 	bytes = bytearray(img.read())
 
-bytes = bytes[HEADER_OFFSET:]
-print(len(bytes))
+# Only take the data from the header onward (pixel data only).
+bytes = bytes[BMP_HEADER_OFFSET:]
+print("Length of pixel data: " + str(len(bytes)))
 
+# Buffer to store parsed decoded results.
 buffer = ""
 
+# Temporary buffer to store current byte.
 bits = ""
+# Parse through all read bytes of pixel data.
 for byte in bytes:
-	if len(bits) != 8:
-		bits += str(byte&1)
-	else:
+	# Get the least significant bit.
+	bits += str(byte & 1)
+	# If the byte is completed, add its ASCII value to the buffer...
+	if len(bits) == 8:
 		char = chr(int(bits, 2))
-		if ord(char) > 32 and ord(char) < 127:
+		# ... only if it is readable.
+		if ord(char) > 32 and ord(char) < 126:
 			buffer += char
+		# And reset the current byte
 		bits = ""
 
-print(buffer)
+print("Result: ")
+print(buffer[:45] + "... *REMAINING RESULTS OMITTED*")
